84e2c1771f5a874f6e35a7bdd3fce84cca259586
mrninhvn <1.ninh.vn@gmail.com>
2020-04-26 00:25:51 +0700

    add function 4

--- main.ts
+++ main.ts
@@ -8,40 +8,5 @@ load dependency
 
 //% color="#a88532" weight=20 icon="\uf281"
 namespace UMI_ML {
-	let c = 0.008
-    let n = 0
-	let weights: number[] = []
-	let inputs: number[] = []
-	c = 0.008
-	inputs = []
-	weights = []
-	n = 0
-
-	//% blockId=perceptron block="Perceptron begin with|%ninputs|input"
-    //% color="#a88532"
-    //% weight=98
-    //% blockGap=10
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-	export function perceptron (ninputs: number) {
-    	n = ninputs
-    	for (let index = 0; index <= n; index++) {
-        	inputs.push(1)
-    	}
-    	for (let index = 0; index <= n; index++) {
-        	weights.push(Math.randomRange(-1000, 1000) / 1000)
-    	}
-	}
-
-	//% blockId=train block="Training with desired value =|%desired|and guess value =|%guess|"
-    //% color="#a88532"
-    //% weight=98
-    //% blockGap=10
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-	export function train (desired: number, guess: number) {
-    	error = desired - guess
-    	for (let index = 0; index <= n; index++) {
-        	weights.push(c * (0 * 0))
-    	}
-	}
-
+	
 }
