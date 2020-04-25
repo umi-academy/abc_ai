7cac97078e83ddf18d71b5436ddb6e53637ae745
mrninhvn <1.ninh.vn@gmail.com>
2020-04-25 13:16:25 +0700

    add c file

---
+++ ai.cpp
@@ -0,0 +1,15 @@
+/*
+Copyright (C): 2020, UMI Academy
+modified by Ninh.D.H
+*/
+
+#include "pxt.h"
+#include <map>
+#include <vector>
+#include "Perceptron.h"
+using namespace pxt;
+
+
+namespace ai {
+
+}
