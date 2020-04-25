25446faacc891e387169c15d70af26e53d3ff1d7
mrninhvn <1.ninh.vn@gmail.com>
2020-04-25 13:35:14 +0700

    fix function 7

--- main.ts
+++ main.ts
@@ -9,39 +9,11 @@ load dependency
 //% color="#a88532" weight=20 icon="\uf281"
 namespace UMI_AI {
 
-    //% blockId=mbit_MotorCtrl block="block 1 $var"
+    //% blockId=mbit_MotorCtrl block="block 1 %var"
     //% weight=4
     //% blockGap=10
     //% color="#a88532"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=1
-    //% expandableArgumentMode="enabled"
     export function MotorCtrl(var: number): void {
-        return
-    }
-
-    //% blockId=mbit_MotorCtrlSpeed block="block 2 $var"
-    //% weight=4
-    //% blockGap=10
-    //% speed.min=0 speed.max=255
-    //% color="#a88532"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=1
-    //% expandableArgumentMode="enabled"
-    export function MotorCtrlSpeed(var: number): void {
-        return
-    }
-
-    //% blockId=mbit_Servo_Car block="block 3 $var"
-    //% weight=96
-    //% blockGap=10
-    //% color="#a88532"
-    //% num.min=1 num.max=3 value.min=0 value.max=180
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=9
-    //% value.shadow="protractorPicker"
-    export function Servo_Car(var: number): void {
-
-        // 50hz: 20,000 us
-        let us = (value * 1800 / 180 + 600); // 0.6 ~ 2.4
-        let pwm = us * 4096 / 20000;
         return
     }
 }
